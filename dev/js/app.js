3f09c7b2ba12aece6058e3b7a5f5845f47cc43c1
Shaun Kelly <shaunkelly@gmail.com>
2016-01-21 22:51:51 -0500

    feat: Add validation module
    Validation module should be usable through all forms. Disables submit
    unless all fields validate. Use custom validity messages.

--- dev/js/app.js
+++ dev/js/app.js
@@ -36,3 +36,63 @@ var FauxBackendModule = function(global) {
 
   return globalAPI;
 };
+
+
+
+var ValidationModule = function() {
+
+  var globalAPI = {};
+  var _cache = {};
+
+  var initValidation = function(form, button) {
+    var formElements = form.getElementsByTagName('input');
+    button.setAttribute('disabled', 'disabled');
+    form.addEventListener('keyup', function(e) {
+      if (_cache.hasOwnProperty(e.target.id) && _cache[e.target.id].haslostfocus) {
+        _setValidityMessage(e.target);
+      }
+      for (var i = 0; i < formElements.length; i++) {
+        if (!formElements[i].checkValidity()) {
+          button.setAttribute('disabled', 'disabled');
+          return;
+        }
+      }
+      button.removeAttribute('disabled');
+    });
+    form.addEventListener('focusout', function(e) {
+      _setValidityMessage(e.target);
+      if (_cache.hasOwnProperty(e.target.id)) {
+        _cache[e.target.id].haslostfocus = true;
+      } else {
+        _cache[e.target.id] = {};
+        _cache[e.target.id].haslostfocus = true;
+      }
+    });
+  };
+
+  var _setValidityMessage = function(inputElement) {
+    var errorField = _getErrorElement(inputElement);
+    if (!inputElement.checkValidity()) {
+      errorField.innerHTML = inputElement.validationMessage;
+    } else if (errorField !== null) {
+      errorField.innerHTML = '';
+    }
+  };
+
+  var _getErrorElement = function(inputNode) {
+    var inputNodeId = inputNode.id;
+    var errorFieldById = document.getElementById(inputNodeId + '-error');
+    // TODO cache error field, maybe, or is that premature optimization?
+    if (inputNodeId && errorFieldById !== null) {
+      return errorFieldById;
+    } else {
+      return inputNode.parentNode.querySelector('.error');
+    }
+  };
+
+  globalAPI = {
+    'initValidation': initValidation
+  };
+
+  return globalAPI;
+};
