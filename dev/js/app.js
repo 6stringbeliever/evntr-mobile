3b1a90c944749a09067acb2cb9200ca2fc49e5e4
Shaun Kelly <shaunkelly@gmail.com>
2016-02-20 16:12:56 -0500

    feat: Get event data from fauxbackend module

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,7 +1,7 @@
 var FauxBackendModule = function(global) {
   'use strict';
 
-  var globalAPI = {},
+  var globalAPI,
       _myStorage = global.localStorage;
 
   /* Constant(likethings) */
@@ -9,31 +9,115 @@ var FauxBackendModule = function(global) {
   var LOGGED_IN_USER = 'logged-in-user';
   var EVENTS = 'all-events';
 
-  globalAPI.isUserLoggedIn = function() {
+  /*
+    Add some faux data if there's no faux data.
+  */
+  function _initialize() {
+    /***
+
+      FIRST LINE IS JUST TO EASE DEVELOPMENT
+
+      DELETE ME WHEN DONE
+
+    ***/
+    _myStorage.clear();
+    var events = getEvents();
+    if (events === null) {
+      events = { events: [{
+        name: 'Porkfest 2016',
+        host: 'Mr. Pig',
+        purpose: 'Pork Festival',
+        date: {
+          startDate: 'March 19, 2016',
+          endDate: 'March 22, 2016',
+          allDay: true
+        },
+        location: {
+          name: 'Grant Park',
+          address: '840 Cherokee Ave SE, Atlanta, GA 30312',
+          picture: null
+        },
+        guests: [],
+        message: 'Eat more pork.'
+      },
+      {
+        name: 'Speed Dating',
+        host: 'Lowered Expectations',
+        purpose: 'Speed Dating',
+        date: {
+          startDate: 'March 19, 2016',
+          startTime: '7:00 PM',
+          endDate: 'March 19, 2016',
+          endTime: '9:00 PM'
+        },
+        location: {
+          name: 'Community Center',
+          address: '123 Fake St., Atlanta, GA 30309',
+          picture: null
+        },
+        guests: ['creepyloner1@gmail.com','creepyloner2@hotmail.com'],
+        message: 'Meet people like you. Parking is $5.'
+      },
+      {
+        name: 'Front-End Developer Meetup',
+        host: 'Front-End Developer Club',
+        purpose: 'Meetup',
+        date: {
+          startDate: 'March 21, 2016',
+          startTime: '8:00 PM',
+          endDate: 'March 21, 2016',
+          endTime: '10:00 PM'
+        },
+        location: {
+          name: 'Octane Coffee Bar',
+          address: '437 Memorial Drive A5, Atlanta, GA 30312'
+        },
+        guests: ['creepyloner1@gmail.com','creepyloner2@hotmail.com'],
+        message: 'We\'ll be in the back room.'
+      }]};
+      _setEvents(events);
+    }
+  }
+
+  function _setEvents(events) {
+    _myStorage.setItem(EVENTS, JSON.stringify(events));
+  }
+
+  function isUserLoggedIn() {
     return (_myStorage.getItem(LOGGED_IN_USER)) ? true : false;
-  };
+  }
 
-  globalAPI.setLoggedInUser = function(user) {
+  function setLoggedInUser(user) {
     _myStorage.setItem(LOGGED_IN_USER, JSON.stringify(user));
-  };
+  }
 
-  globalAPI.getLoggedInUser = function() {
+  function getLoggedInUser() {
     return JSON.parse(_myStorage.getItem(LOGGED_IN_USER));
-  };
+  }
 
-  globalAPI.logOutUser = function() {
+  function logOutUser() {
     _myStorage.removeItem(LOGGED_IN_USER);
-  };
+  }
 
-  globalAPI.addEvent = function(e) {
-    var events = this.getEvents();
-    events.push(e);
-  };
-
-  globalAPI.getEvents = function() {
+  function getEvents() {
     return JSON.parse(_myStorage.getItem(EVENTS));
+  }
+
+  function addEvent(e) {
+    var events = getEvents();
+    events.events.push(e);
+  }
+
+  globalAPI = {
+    getEvents: getEvents,
+    addEvent: addEvent,
+    logOutUser: logOutUser,
+    getLoggedInUser: getLoggedInUser,
+    setLoggedInUser: setLoggedInUser,
+    isUserLoggedIn: isUserLoggedIn
   };
 
+  _initialize();
   return globalAPI;
 };
 
