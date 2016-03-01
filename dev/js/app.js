13eab349846f89fb9eb9b29fb9ad1e3e6b1b4ab3
Shaun Kelly <shaunkelly@gmail.com>
2016-02-29 21:21:08 -0500

    feat: Add logged in and log out modules and styles
    Logged in module checks the faux backend if there is a logged in
    user. It applies a style to root if there is. Added styles to css
    to take advantage and hide menu options based on logged in status.
    
    Log out module logs out the user when you click the specified item
    then updates the styles.

--- dev/js/app.js
+++ dev/js/app.js
@@ -539,6 +539,40 @@ var FauxBackendModule = function(global) {
   self.fetch.polyfill = true
 })(typeof self !== 'undefined' ? self : this);
 
+/*
+  Module to set a class on the html root if a user is logged in. Pass
+  in the root element, a function to call to verify if a user is logged
+  in and an optional class name to apply to the root element.
+*/
+var LoggedIn = function(root, loggedIn, clin, clout) {
+
+  var isLoggedIn;
+  var clInName = clin || 'logged-in';
+  var clOutName = clout || 'logged-out';
+  update();
+
+  function update() {
+    isLoggedIn = loggedIn();
+    if (isLoggedIn) {
+      root.classList.add(clInName);
+      root.classList.remove(clOutName);
+    } else {
+      root.classList.add(clOutName);
+      root.classList.remove(clInName);
+    }
+  }
+
+  return { update: update };
+};
+
+var LogoutButton = function(lobtn, backend, loginmodule) {
+  lobtn.addEventListener('click', function(e) {
+    backend.logOutUser();
+    loginmodule.update();
+    e.preventDefault();
+  });
+};
+
 /*!
  * mustache.js - Logic-less {{mustache}} templates with JavaScript
  * http://github.com/janl/mustache.js
